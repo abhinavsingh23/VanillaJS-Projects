fec85b14df7f4281b4be9e99863607ae49c134d9
abhinav <abhinavsingh2902@gmail.com>
2020-06-16 18:34:13 +0530

    Adding Dice

--- Pig Game(Basic DOM Manipulation)/app.js
+++ Pig Game(Basic DOM Manipulation)/app.js
@@ -15,9 +15,21 @@ scores = [0, 0];
 roundScore = 0;
 activePlayer = 1; //First Player-> 0 and Second Player =1
 
-dice = Math.floor(Math.random() * 6) + 1;
-
 // document.querySelector("#current-" + activePlayer).textContent = dice;
 
-document.querySelector("#current-" + activePlayer).innerHTML =
-  "<em>" + dice + "</em>";
+// document.querySelector("#current-" + activePlayer).innerHTML =
+//   "<em>" + dice + "</em>";
+
+// var x = document.querySelector('#score-0').textcontent
+
+document.querySelector(".dice").style.display = "none";
+
+document.querySelector(".btn-roll").addEventListener("click", function () {
+  var dice = Math.floor(Math.random() * 6) + 1;
+
+  var diceDOM = document.querySelector(".dice");
+
+  diceDOM.style.display = "block";
+
+  diceDOM.src = "dice-" + dice + ".png";
+});
